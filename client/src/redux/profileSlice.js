f4f5fd9b4cd9766f4162f76187e807656febbab8
Umar-Akmal <umar.akmal@cogenteservices.com>
2023-12-27 12:24:05 +0530

    first

---
+++ client/src/redux/profileSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    showProfile: false,
+    showNotifications: false,
+};
+// const profileSlice = createSlice({
+//     name: 'profile',
+//     initialState,
+//     reducers: (builder) => {
+//         builder
+//             .addCase(setShowProfile, (state, { payload }) => {
+//                 state.showProfile = payload;
+//             })
+//             .addCase(setShowNotifications, (state, { payload }) => {
+//                 state.showNotifications = payload;
+//             });
+//     },
+// });
+
+const profileSlice = createSlice({
+    name: "profile",
+    initialState,
+    reducers: {
+        setShowProfile: (state, { payload }) => {
+            state.showProfile = payload;
+        },
+        setShowNotifications: (state, { payload }) => {
+            state.showNotifications = payload;
+        },
+    },
+});
+export const { setShowProfile, setShowNotifications } = profileSlice.actions;
+export default profileSlice.reducer;
